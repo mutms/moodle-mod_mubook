d97ad22223949df047586c66cf6b0222b8cfa577
Petr Skoda <petr@mutms.org>
2025-11-08 13:13:05 +0100

    Prepare preview release

--- CHANGES.md
+++ CHANGES.md
@@ -2,6 +2,6 @@
 
 ## mu-5.0.3-00
 
-Release date: xx/11/2025
+Release date: 08/11/2025
 
 Initial preview release of new Interactive book module.
--- version.php
+++ version.php
@@ -28,12 +28,12 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_mubook';
-$plugin->version = 2025103150;
+$plugin->version = 2025110850;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [500, 501];
 $plugin->release = 'mu-5.0.3-00';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025100650.01,
+    'tool_mulib' => 2025110850,
 ];
