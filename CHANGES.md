83d988c62a93ccd68dc82449d477a9fa26c62443
Petr Skoda <petr@mutms.org>
2025-12-02 13:06:56 +0100

    Update use of sql fragments

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## mu-4.5.7-03
+
+Release date: xx/11/2025
+
+* Updated use of sql fragments API.
+
 ## mu-4.5.7-01
 
 Release date: 14/11/2025
--- version.php
+++ version.php
@@ -28,12 +28,12 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_mubook';
-$plugin->version = 2025111445;
+$plugin->version = 2025112745;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release = 'mu-4.5.7-01';
+$plugin->release = 'mu-4.5.7-02+';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025110845,
+    'tool_mulib' => 2025112745,
 ];
