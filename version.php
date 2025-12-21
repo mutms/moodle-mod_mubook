945eedc0c979b2413d07e27a79ba7d8678919f69
Petr Skoda <petr@mutms.org>
2025-12-21 17:43:11 +0100

    Bump version

--- version.php
+++ version.php
@@ -28,12 +28,12 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_mubook';
-$plugin->version = 2025121645;
+$plugin->version = 2025122145;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
 $plugin->release = 'mu-4.5.8-02+';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025121645,
+    'tool_mulib' => 2025122145,
 ];
