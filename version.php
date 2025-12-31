55c1171289e1974c4916e6cf7c1f9b3ba0e3d1f4
Petr Skoda <petr@mutms.org>
2025-12-31 10:46:36 +0100

    Revert versioning changes

--- version.php
+++ version.php
@@ -28,12 +28,12 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_mubook';
-$plugin->version = '2025123145';
+$plugin->version = 2025123145;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
 $plugin->release = 'mu-4.5.8-03';
 
 $plugin->dependencies = [
-    'tool_mulib' => '2025123145',
+    'tool_mulib' => 2025123145,
 ];
