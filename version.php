22ea37303e679dd57647081fcb6ad1bd146f21a3
Petr Skoda <petr@mutms.org>
2026-08-08 11:20:37 +0000

    Back to dev

--- version.php
+++ version.php
@@ -31,7 +31,7 @@ $plugin->component = 'mod_mubook';
 $plugin->version = 2026060545;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
-$plugin->release = 'v4.5.12.01';
+$plugin->release = 'v4.5.12.01+';
 
 $plugin->dependencies = [
     'tool_mulib' => 2026060545,
