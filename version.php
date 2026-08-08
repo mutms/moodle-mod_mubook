2198bb938e251746af081202eddf50d4d1d8b7af
Petr Skoda <petr@mutms.org>
2026-08-08 20:01:47 +0000

    Prepare release

--- version.php
+++ version.php
@@ -28,11 +28,11 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_mubook';
-$plugin->version = 2026060545;
+$plugin->version = 2026080845;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->release = 'v4.5.13.01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026060545,
+    'tool_mulib' => 2026080845,
 ];
